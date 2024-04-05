7d738acc8a9154ac9773bd74a839a7b358952a8b
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-04-06 01:58:32 +0200

    Created fmt object to format repl inputs

--- lua/fmt.lua
+++ lua/fmt.lua
@@ -1,12 +1,50 @@
----@class Text
-local Text = {}
+local M = {}
+
+---@class Fmt
+---@field lines table
+local Fmt = {}
 
 ---Class contructor
 ---@param content string
----@return Text
-function Text:new(content)
-	local obj = { content = content }
+---@return Fmt
+function Fmt:new(content)
+	local lines = vim.split(content, "\n")
+	local obj = { lines = lines }
 	self.__index = self
 
 	return setmetatable(obj, self)
 end
+
+---Strip lines from whitespace
+---@return Fmt
+function Fmt:strip()
+	local lines = {}
+	for _, line in ipairs(self.lines) do
+		table.insert(lines, vim.trim(line))
+	end
+
+	return Fmt:new(table.concat(lines, "\n"))
+end
+
+---Strip lines from newlines
+---@return Fmt
+function Fmt:nonewline()
+	local lines = {}
+	for _, line in ipairs(self.lines) do
+		if vim.trim(line) ~= "" then
+			table.insert(lines, line)
+		end
+	end
+
+	return Fmt:new(table.concat(lines, "\n"))
+end
+
+---Return stringified content
+---@return string
+function Fmt:string()
+	return table.concat(self.lines, "\n")
+end
+
+M.Fmt = Fmt
+
+return M
