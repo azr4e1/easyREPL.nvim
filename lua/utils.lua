0a213a855345f35a42eb8e24622b9524dfc68a1b
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-04-07 01:23:49 +0200

    Added get text functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -31,7 +31,7 @@ function M.get_term_size_pct(pct)
 	return termHeight, termWidth
 end
 
----comment
+---Get anchor for floating window
 ---@param termHeight number
 ---@param termWidth number
 ---@return number, number
@@ -52,6 +52,33 @@ function M.get_centre_pos_float(termHeight, termWidth)
 	end
 
 	return row, col
+end
+
+---Get current line as text
+---@return string
+function M.get_current_line()
+	return vim.api.nvim_ge_current_line()
+end
+
+---Get current visual selection as text. From https://neovim.discourse.group/t/function-that-return-visually-selected-text/1601
+---@return string
+function M.get_current_selection()
+	local s_start = vim.fn.getpos("'<")
+	local s_end = vim.fn.getpos("'>")
+	local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+	local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+	lines[1] = string.sub(lines[1], s_start[3], -1)
+	if n_lines == 1 then
+		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+	else
+		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+	end
+	return table.concat(lines, "\n")
+end
+
+function M.get_current_buffer()
+	local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
+	return table.concat(content, "\n")
 end
 
 return M
