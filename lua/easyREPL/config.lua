54457257242d6b1c8f09305fe2f6aaa0445c3dd9
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-04-12 18:25:44 +0100

    feat(config): added filetype field to config which respects order

--- lua/easyREPL/config.lua
+++ lua/easyREPL/config.lua
@@ -5,6 +5,24 @@ local u = require("easyREPL.utils")
 
 local M = {}
 
+---Update filetype table
+---@param repl_name string
+---@param old_ft table<string>
+local function update_filetypes(repl_name, old_ft, new_ft)
+	if new_ft == nil or type(new_ft) ~= "table" then
+		return
+	end
+
+	for _, ft in ipairs(new_ft) do
+		local repls = u.copy(old_ft[ft])
+		if repls == nil then
+			repls = {}
+		end
+		table.insert(repls, repl_name)
+		old_ft[ft] = repls
+	end
+end
+
 ---@class Config
 ---@field repls table<Repl>
 ---@field default_repl table
@@ -21,6 +39,7 @@ function Config:new(opts)
 	obj.default_term = u.get_defaults(opts, defaults.term_defaults)
 
 	local repls = {}
+	local filetypes = {}
 	if type(opts["repls"]) == "table" then
 		for _, repl_conf in ipairs(opts["repls"]) do
 			local ok, current_repl = pcall(function()
@@ -32,11 +51,14 @@ function Config:new(opts)
 			end
 			local current_term_conf = u.get_defaults(repl_conf, obj.default_term)
 			repls[current_repl.name] = { repl_config = current_repl, term_config = current_term_conf }
-		    ::continue::
+
+			update_filetypes(current_repl.name, filetypes, current_repl.filetypes)
+			::continue::
 		end
 	end
 
 	obj.repls = repls
+	obj.filetypes = filetypes
 
 	self.__index = self
 	return setmetatable(obj, self)
